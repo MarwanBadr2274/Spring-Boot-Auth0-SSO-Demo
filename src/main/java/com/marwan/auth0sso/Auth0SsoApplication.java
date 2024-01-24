d4d00b7b31a1e571f576dded6744f26c3a9e27b3
marwanBadr14 <marwan.badr@gizasystems.onmicrosoft.com>
2024-01-24 22:12:57 +0200

    Local commit 1 - everything sorted

---
+++ src/main/java/com/marwan/auth0sso/Auth0SsoApplication.java
@@ -0,0 +1,13 @@
+package com.marwan.auth0sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth0SsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auth0SsoApplication.class, args);
+	}
+
+}
